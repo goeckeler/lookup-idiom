f8233f0ca3a9765687ae8a161c52deedcdf3cc87
Thorsten Göckeler <thorsten.goeckeler@iteratec.com>
2020-05-13 18:04:02 +0200

    Explizites Interface ersetzt durch generisches Interface.

--- src/demo/Calculator.java
+++ src/demo/Calculator.java
@@ -1,33 +1,29 @@
 package demo;
 
 import java.util.Map;
+import java.util.function.BinaryOperator;
 
 public class Calculator
 {
   public static double calculate(final String operator, final double a, final double b) {
-    return Lookup.operator(operator).calculate(a, b);
+    return Lookup.operator(operator).apply(a, b);
   }
 }
 
-interface Operation
-{
-  double calculate(double a, double b);
-}
-
-class Plus implements Operation
+class Plus implements BinaryOperator<Double>
 {
   @Override
-  public double calculate(final double a, final double b) {
+  public Double apply(final Double a, final Double b) {
     return a + b;
   }
 }
 
 class Lookup
 {
-  static final Map<String, Operation> lookup = Map.of("+", new Plus());
+  static final Map<String, BinaryOperator<Double>> lookup = Map.of("+", new Plus());
 
-  static Operation operator(final String operator) {
-    final Operation operation = lookup.get(operator);
+  static BinaryOperator<Double> operator(final String operator) {
+    final BinaryOperator<Double> operation = lookup.get(operator);
 
     if (operation == null) {
       throw new IllegalArgumentException(String.format("Operator '%s' not supported.", operator));
