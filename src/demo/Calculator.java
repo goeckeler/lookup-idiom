edc0a5c57e6ccadb29b5f86375b2591f89deaca8
Thorsten Göckeler <thorsten.goeckeler@iteratec.com>
2020-05-13 17:35:57 +0200

    Methode als Class umgewandelt

--- src/demo/Calculator.java
+++ src/demo/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator
   public static double calculate(final String operator, final double a, final double b) {
     switch (operator) {
       case "+" :
-        return a + b;
+        return new Plus().calculate(a, b);
       case "-" :
         return a - b;
       case "*" :
@@ -17,5 +17,18 @@ public class Calculator
       default :
         throw new IllegalArgumentException(String.format("Operator '%s' is not supported.", operator));
     }
+  }
+}
+
+interface Operation
+{
+  double calculate(double a, double b);
+}
+
+class Plus implements Operation
+{
+  @Override
+  public double calculate(final double a, final double b) {
+    return a + b;
   }
 }
